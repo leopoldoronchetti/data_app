38a9394a3933a9a2c3b630376123544136c6c1b6
leopoldoronchetti <leopoldo.r2000@gmail.com>
2026-04-17 17:51:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import yfinance as yf
+import matplotlib.pyplot as plt
 
-st.title("This is a data app!!!")
-st.write("Work in progress!")
+# Title
+st.title("Apple Stock Time Series")
+
+# Fetch data for Apple (AAPL)
+ticker = "AAPL"
+data = yf.download(ticker, period="1y")
+
+# Show raw data
+st.subheader("Raw Data")
+st.write(data.tail())
+
+# Plot closing price
+st.subheader("Closing Price Over Time")
+
+fig, ax = plt.subplots()
+ax.plot(data.index, data["Close"])
+ax.set_xlabel("Date")
+ax.set_ylabel("Price (USD)")
+ax.set_title("AAPL Closing Price")
+
+st.pyplot(fig)
