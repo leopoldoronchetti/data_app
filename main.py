f855163de51767976e7b80383c7599b5dd423468
leopoldoronchetti <leopoldo.r2000@gmail.com>
2026-04-20 19:44:57 +0200

    Update main.py (if name == main removed)

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import streamlit as st
 from apps.app_finance import app_finance
 from components.login import login_page
 
-if __name__ == "__main__":
 
-    st.set_page_config(layout="wide")
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
-    if not st.session_state.logged_in:
-        login_page()
-    elif "logged_in" in st.session_state:
-        app_finance()
+st.set_page_config(layout="wide")
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+if not st.session_state.logged_in:
+    login_page()
+elif "logged_in" in st.session_state:
+    app_finance()
 
 
 
